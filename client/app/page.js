b37313c4cb8408222a9c4b478551ca6ae46a4835
Abdul Ghaffar <agscontact777@gmail.com>
2025-07-25 11:15:43 +0500

    Login page ui done

--- client/app/page.js
+++ client/app/page.js
@@ -1,103 +1,59 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="w-screen">
+      <div className="max-w-[380px] w-full bg-light border-borderc border rounded-md mt-5 shadow-md mx-auto p-4">
+        {/* Heading of login */}
+        <h1 className="text-3xl text-primary text-center font-bold ">Login</h1>
+        {/* Form  */}
+        <form>
+          {/* email  */}
+          <div className="my-4">
+            <label className="text-md text-textc cursor-pointer select-none">Email</label>
+            <input type="email" className="w-full px-4 py-2 text-md outline-none border border-borderc shadow-sm text-dark rounded-md" />
+          </div>
+          {/* password  */}
+          <div className="mb-4">
+            <label className="text-md text-textc cursor-pointer select-none">Password</label>
+            <input type="password" className="w-full px-4 py-2 text-md outline-none border border-borderc shadow-sm text-dark rounded-md" />
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+          <div>
+            {/* Remember me and forgot password */}
+            <div className="flex justify-between items-center mb-4 h-fit">
+              <div className="flex items-center">
+                <input id="rememberme" type="checkbox" className="h-4 w-4 mr-2 text-primary accent-primary  rounded focus:ring-primary focus:outline-none" />
+                <label className="text-sm text-textc cursor-pointer select-none" htmlFor="rememberme">Remember me</label>
+              </div>
+              <Link href="/forgot-password" className="text-sm text-primary hover:text-primary-hover select-none" >Forgot password?</Link>
+            </div>
+
+            {/* Login button */}
+            <button type="submit" className="w-full px-4 py-2 bg-primary text-light rounded-md hover:bg-primary-hover">Login</button>
+
+            {/* other options */}
+            <div>
+              {/* heading */}
+              <div className="relative flex items-center justify-center my-5 bg-red-600">
+                <hr className="text-borderc top-0 w-full"/>
+                <h1 className="absolute text-textc text-sm bg-bgc px-2 text-textc">Or login with</h1>
+              </div>
+              <div className="flex justify-between items-center mt-2 gap-2">
+                <Link href="/login/google" className="w-full px-4 py-2 bg-white text-dark border border-borderc rounded-md hover:bg-gray-100 text-center">Google</Link>
+                <Link href="/login/github" className="w-full px-4 py-2 bg-white text-dark border border-borderc rounded-md hover:bg-gray-100 text-center">GitHub</Link>
+                </div>
+            </div>
+
+            {/* Register link */}
+            <div className="mt-4 text-center">
+              <span className="text-sm text-textc">Don't have an account? </span>
+              <Link href="/register" className="text-sm text-primary hover:text-primary-hover">Signup</Link>
+            </div>
+
+          </div>
+
+        </form>
+      </div>
+    </div>);
 }
---
+++ server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func test(name string){
+	fmt.Println("Working just fine test")
+	fmt.Println("Hello, " + name)
+}
+
+
+func main(){
+	test("Abdul Ghaffar")
+}
